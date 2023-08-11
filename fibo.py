aaaa11fdbe679c87e09b4d57f7887f198cf72f27
Samuel Karmole <sambeti2002@gmail.com>
2023-08-11 00:50:48 -0700

    This is My first commit message for python

---
+++ fibo.py
@@ -0,0 +1,14 @@
+# fibonacci series module
+def fibonacci(n):
+    a, b =0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+        print()
+        def fibonacci2(n):
+            result = []
+            a, b = 0, 1
+            while a < n:
+                result.append(a)
+                a, b = b, a + b
+                return result
